3ea06702319db73fb1027f029a450d7f68e48972
arthurtng <darkmatter2000@gmail.com>
2023-07-09 05:02:47 +0100

    add exercise 1.1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+const Header = (props) => {  
+  console.log('header: ' + props.course)
+
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {    
+  for (let i=0; i < props.list.length; i++){
+    console.log('content: ' + props.list[i]['part'] + ' ' + props.list[i]['number'])
+  }
+
+  const items = props.list.map(p => <p>{p['part']} {p['number']}</p>)
+      
+  return (
+    <>
+      {items}
+    </>
+  )
+}
+
+const Total = (props) => {
+  let sum = 0
+  for (let i=0; i < props.list.length; i++){
+    sum += props.list[i]['number']
+  }
+
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+
+}
+
+
+const App = () => {
+  
+  const course = 'Half Stack application development'
+
+  const content = [
+    { 
+      part: 'Fundamentals of React',
+      number: 10
+    },
+    { 
+      part: 'Using props to pass data',
+      number: 7
+    },
+    { 
+      part: 'State of a component',
+      number: 14
+    }
+  ]    
+
+  return (
+    <div>
+      <Header course={course}/>      
+      <Content list={content} /> 
+      <Total list={content} />
+    </div>
+  )
+}
+
+export default App
+
